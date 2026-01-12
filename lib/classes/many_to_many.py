ee7e15426e417588a2bcce71a89d8c836f4b85f6
commit-panda <sjmanyarkiy@gmail.com>
2026-01-12 17:20:12 +0300

    Article is initialized with a title

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,37 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if hasattr(self, '_title'):
+            return
+        if isinstance(value, str) and 5 <= len(value) <=50:
+            self._title = value
         
 class Author:
     def __init__(self, name):
         self.name = name
+
+    @property
+    def name(self):
+        return self._name 
+    
+    @name.setter
+    def name(self, value):
+        if hasattr(self, '_name'):
+            return
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
 
     def articles(self):
         pass
